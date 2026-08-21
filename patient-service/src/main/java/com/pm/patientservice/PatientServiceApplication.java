e91b5d08c6e3318a8d21b97acec5f2e13a3c6325
zhaidersmd <zhaidersmd@gmail.com>
2026-08-21 22:48:46 +0530

    Add Jenkins CI pipeline

--- patient-service/src/main/java/com/pm/patientservice/PatientServiceApplication.java
+++ patient-service/src/main/java/com/pm/patientservice/PatientServiceApplication.java
@@ -2,13 +2,22 @@ package com.pm.patientservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class PatientServiceApplication {
 
     public static void main(String[] args) {
 
         SpringApplication.run(PatientServiceApplication.class, args);
     }
+
+    @GetMapping("/")
+    public String welcomePage() {
+        return "Welcome to Patient Service Module";
+    }
+
 
 }
--- patient-service/src/main/java/com/pm/patientservice/model/Patient.java
+++ patient-service/src/main/java/com/pm/patientservice/model/Patient.java
@@ -7,7 +7,7 @@ import java.time.LocalDate;
 import java.util.UUID;
 
 @Entity
-@Table(name = "PATIENT")
+@Table(name = "Patient")
 public class Patient {
 
     @Id
